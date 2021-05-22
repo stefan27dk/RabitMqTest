dd3ac0cb8b11e2c4e3705dec63e3def2aec95e3d
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-05-22 13:54:26 +0200

    Add project files.

---
+++ Receive/Receive.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+class Receive
+{
+    public static void Main()
+    {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.UserName = "user";
+        factory.Password = "password";
+        factory.VirtualHost = "/";
+        factory.HostName = "localhost";
+        factory.Port = AmqpTcpEndpoint.UseDefaultPort;
+        IConnection connection = factory.CreateConnection();
+
+
+        //using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class Send
+{
+    public static void Main()
+    {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.UserName = "user";
+        factory.Password = "password";
+        factory.VirtualHost = "/";
+        factory.HostName = "localhost";
+        factory.Port = AmqpTcpEndpoint.UseDefaultPort;
+        IConnection connection = factory.CreateConnection();
+
+        //using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            string message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
