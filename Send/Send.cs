1fc16579ee38e415d0663998448922180c989719
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-05-24 12:19:51 +0200

    While true send messages

--- Send/Send.cs
+++ Send/Send.cs
@@ -15,19 +15,23 @@ class Send
         factory.Port = AmqpTcpEndpoint.UseDefaultPort;
         IConnection connection = factory.CreateConnection();
 
+        while(true)
+        {
+
         //using (var connection = factory.CreateConnection())
         using (var channel = connection.CreateModel())
         {
             channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
-            string message = "Hello World!";
+            string message = Console.ReadLine();
             var body = Encoding.UTF8.GetBytes(message);
 
             channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
             Console.WriteLine(" [x] Sent {0}", message);
         }
 
-        Console.WriteLine(" Press [enter] to exit.");
-        Console.ReadLine();
+        //Console.WriteLine(" Press [enter] to exit.");
+        //Console.ReadLine();
+        }
     }
 }
